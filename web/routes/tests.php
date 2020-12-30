85db9ba6f0be28841a91ccb72c6540775f3f70d2
yuriy <bibrkacity@gmail.com>
2020-12-30 15:40:31 +0200

    Unit-test

--- web/routes/tests.php
+++ web/routes/tests.php
@@ -10,8 +10,8 @@ $router->get('/tests/payments/payments/system/index', function () {
     return \App\Http\Controllers\TestController::viewMake("tests.payment.system.index");
 });
 
-$router->get('/tests/payments/payments/invoices/charge', function () {
-    return \App\Http\Controllers\TestController::viewMake("tests.payment.invoice.bitpay.store");
+$router->get('/tests/payments/payments/invoices/charge/', function () {
+    return \App\Http\Controllers\TestController::viewMake("tests.payment.invoice.store");
 });
 
 /* =================
